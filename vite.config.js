c19fe1f3507b3fa45e6b5bf3c381032d85cf0db0
Smadhusanka <smadusanka2001@gmail.com>
2024-10-22 23:10:02 +0530

    change vite.config.js and deploy yml

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/testproject/"
+  base: process.env.NODE_ENV === 'production' && process.env.DEPLOY_PLATFORM === 'github'
+  ? '/testproject/'
+  : '/',
 })
